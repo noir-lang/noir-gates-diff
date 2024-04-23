eee534088d3b3092030e953b35831ac1ae9fd7e7
vezenovm <mvezenov@gmail.com>
2024-04-23 17:24:41 +0000

    change circuit name to 1_mul test

--- src/index.ts
+++ src/index.ts
@@ -114,7 +114,7 @@ async function run() {
     core.info(`Mapping reference gas reports`);
     core.info(`Making dummy reference report`);
     const referenceReports = compareReports.programs.map((program) => {
-      const circuitReport = { name: "main", acir_opcodes: 0, circuit_size: 0 };
+      const circuitReport = { name: "1_mul", acir_opcodes: 0, circuit_size: 0 };
       const programReport = {
         name: program.name,
         functions: [circuitReport],
