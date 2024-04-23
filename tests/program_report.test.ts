de3e1c7fb2af3f749e7c6be1a6a3479e8426a6fd
vezenovm <mvezenov@gmail.com>
2024-04-23 19:18:51 +0000

    fresh report test

--- tests/program_report.test.ts
+++ tests/program_report.test.ts
@@ -72,37 +72,34 @@ describe("Shell format", () => {
   });
 
   it("should compare 1 to 2", () => {
-    const srcContractReports = cmpContractReports.map((program) => {
-      const circuitReport = { name: "main", acir_opcodes: 0, circuit_size: 0 };
-      const programReport = {
-        package_name: program.package_name,
-        functions: [circuitReport],
-      };
-      return programReport;
-    });
+    // const srcContractReports = cmpContractReports.map((program) => {
+    //   const circuitReport = { name: "main", acir_opcodes: 0, circuit_size: 0 };
+    //   const programReport = {
+    //     package_name: program.package_name,
+    //     functions: [circuitReport],
+    //   };
+    //   return programReport;
+    // });
     const contractDiffs = computeProgramDiffs(srcContractReports, cmpContractReports);
-    console.log(`Format markdown of ${contractDiffs.length} diffs`);
     expect(contractDiffs.length).toBeGreaterThan(0);
 
     console.log(formatShellDiff(contractDiffs));
   });
 
-  it("should compare fresh full report", () => {
-    const cmpContent = fs.readFileSync("tests/mocks/full_gas_report.json", "utf8");
-    const cmpContractReports = loadReports(cmpContent).programs;
+  // This test is just to make sure that we are accurately resetting our reference
+  // report in case it gets malformed
+  it("should compare fresh report", () => {
     const srcContractReports = cmpContractReports.map((program) => {
-      const circuitReport = { name: "main", acir_opcodes: 0, circuit_size: 0 };
+      const circuitReport = { name: "main", acir_opcodes: 1, circuit_size: 1 };
       const programReport = {
         package_name: program.package_name,
         functions: [circuitReport],
       };
       return programReport;
     });
-    console.log(`Source report length: ${srcContractReports.length}`);
-    console.log(`Compare report length: ${cmpContractReports.length}`);
     const contractDiffs = computeProgramDiffs(srcContractReports, cmpContractReports);
-    console.log(`Format markdown of ${contractDiffs.length} diffs`);
     expect(contractDiffs.length).toBeGreaterThan(0);
+
     console.log(formatShellDiff(contractDiffs));
   });
 
